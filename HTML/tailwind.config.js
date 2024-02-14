c2da8a666657b99fc2e153faddc9905b979f3cbd
wience <wbdelafuente@up.edu.ph>
2024-02-14 10:40:28 +0800

    changes

---
+++ HTML/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+    ],
+    darkMode: ['class'],
+    theme: {
+
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '1rem',
+                md: '1rem',
+                lg: '1rem',
+                xl: '3rem',
+                '2xl': '14rem',
+            },
+        },
+
+        fontFamily: {
+            'body': ['Nunito', 'sans-serif'],
+        },
+    },
+}
